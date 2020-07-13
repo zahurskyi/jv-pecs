b8b6ffa17d4c633a285ecba138419ecd161c61b2
Bohdan Chupika <boroda4436@users.noreply.github.com>
2020-07-13 17:53:44 +0300

    Update MachineProducer.java (#2)

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -1,12 +1,11 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.Machine;
 import java.util.List;
 
-public interface MachineProducer<T extends Machine> {
+public interface MachineProducer {
     /**
      * In your implementations - create 2-3 models, add them to the list and return
      * @return - the list of models
      */
-    List<T> get();
+    List<Object> get();
 }
